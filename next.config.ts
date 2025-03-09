bb43e1c14cb3486b6fa110c98b99d6314a28c613
ProgrammerYearsh <programmer.yearsh@gmail.com>
2025-03-09 20:37:34 +0530

    next config updated to work in amplify

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  target: 'experimental-serverless-trace', 
   typescript: {
     ignoreBuildErrors: true,
   },
