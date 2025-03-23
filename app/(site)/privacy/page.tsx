b4f7e5103ec297a6cf87506445c2f696dbd12177
ProgrammerYearsh <programmer.yearsh@gmail.com>
2025-03-23 22:55:10 +0530

    github action config update -v1.3

--- app/(site)/privacy/page.tsx
+++ app/(site)/privacy/page.tsx
@@ -7,7 +7,7 @@ import React from 'react'
 function Privacy() {
   return (
     <SectionHeader classes="py-30  px-20">
-    <span className='text-3xl font-semibold'>Content in progress!</span>
+    <span className='text-3xl font-semibold'>Content in progress!!!</span>
     </SectionHeader>
   )
 }
