0cc7819a1df908c7238b34fbfd0af14290e3cd56
ProgrammerYearsh <programmer.yearsh@gmail.com>
2025-03-24 14:05:01 +0530

    connected aws rds mysql db

--- app/(site)/test/page.tsx
+++ app/(site)/test/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Test() {
   return (
      <SectionHeader classes="py-30  px-20">
-       <span className='text-3xl font-semibold'>Welcome to testing page!</span>
+       <span className='text-3xl font-semibold'>Welcome to testing page! Check for db</span>
        </SectionHeader>
   )
 }
